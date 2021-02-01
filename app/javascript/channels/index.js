a1dbe2415f24a580506bec191cc64f98aecddd41
Jess Pring <jessicapring@outlook.com>
2021-02-01 18:57:24 +0000

    js header animation

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,18 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 2200,
+    delay: (el, i) => 500 + 30 * i
+  });
+
